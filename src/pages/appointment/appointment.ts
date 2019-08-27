3283062223df9e18d65425a41781a14bf500aa99
Heumsinat <heumsinatgic25@gmail.com>
2019-08-27 12:55:08 +0700

    add appointment

--- src/pages/appointment/appointment.ts
+++ src/pages/appointment/appointment.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { ServicesProvider } from '../../providers/services/services';
+import {BarcodeScannerOptions} from '@ionic-native/barcode-scanner';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
+import { HttpClient } from '@angular/common/http';
+
+let apiUrl = 'http://178.128.100.177/securitysys';
 
 /**
  * Generated class for the AppointmentPage page.
@@ -14,12 +20,31 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'appointment.html',
 })
 export class AppointmentPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  public infoID;
+  items: any = [];
+  public Data = { id:"", company:"", purpose_id:"",time:"" , date:"", contact:"", department:"",inf_tel:"", reason:"", finish:""};
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    private service: ServicesProvider,
+    public http: HttpClient,
+    private barcodeScanner: BarcodeScanner) {
+      typeof this.navParams.get('infoID') == 'undefined' ? this.infoID = 'root' : this.infoID = this.navParams.get('infoID');
+      this.getAppointment();
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AppointmentPage');
+  }
+
+  getAppointment(){
+    this.service.getData('/api/appointment').then((result)=>{
+      this.items = result; 
+      console.log ('List of getAppointment', this.items);
+      localStorage.setItem('appointment', this.items);
+    },(err) => {
+      // console.log(err);
+    });
   }
 
 }
